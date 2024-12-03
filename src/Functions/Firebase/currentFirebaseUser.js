7e5e0ef96983cc9ec6fb43c6e7ccb27dc7fa12ce
Oswold Saraki <86856265+darcy561@users.noreply.github.com>
2024-12-03 23:08:23 +0000

    commit

--- src/Functions/Firebase/currentFirebaseUser.js
+++ src/Functions/Firebase/currentFirebaseUser.js
@@ -1,9 +1,9 @@
 import { usersDefault } from "../../Context/defaultValues";
 import { auth } from "../../firebase";
 
-export function getCurrentFirebaseUser() {
+function getCurrentFirebaseUser() {
   const user = auth.currentUser;
-  return user ? user.uid : usersDefault.accountID;
+  return user?.uid || usersDefault[0].accountID;
 }
 
 export default getCurrentFirebaseUser;
